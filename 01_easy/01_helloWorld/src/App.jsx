cb0272611a01ac7b41fbb8bb53404a2e9eb215a8
Deepak Kumar Prajapati <deepakpk756@gmail.com>
2024-01-26 14:21:42 +0530

    first react project

---
+++ 01_easy/01_helloWorld/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import './assets/hello.svg';
+
+function App() {
+
+  return (
+    <div className='mainContainer'>
+      <div className='container'>
+        <h1>Hello World</h1>
+        <img src='hello.svg' alt='logo'/>
+      </div>
+
+      <footer>&copy; Deepak Prajapati</footer>
+    </div>
+  )
+}
+
+export default App
